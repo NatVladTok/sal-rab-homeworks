5b06279090dc04d5f5c4ed7933f894c19c6c4020
Nat <raspopovanv@mail.ru>
2022-07-05 22:17:09 +0700

    2rabota

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,6 +2,18 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    let shippingSum;
+
+    if (productsSum == 0) {
+        shippingSum = 0;
+    } 
+      else  if (productsSum >= freeShippingMinSum) {
+            shippingSum = 0;               
+        }
+        else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
+
 
     // Задание №2.1. Рассчитать доставку
 
@@ -25,6 +37,12 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+    let discountSum;
+
+    if (productsSum >= discountMinSum){
+        discountSum = discountPart/100  * productsSum;
+    }
+      else discountSum = 0;
 
     // Задание №2.2. Рассчитать скидку
 
@@ -42,6 +60,10 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum;
+
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -52,7 +74,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
+    totalSum = totalSum + shippingSum;
+
     // прибавьте к totalSum значение shippingSum
+
+    let freeShipping;
+    freeShipping = !shippingSum;
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
